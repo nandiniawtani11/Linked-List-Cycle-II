d424920480a06b36e1c98d86fe443fd4d19a0cf3
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-24 12:46:05 +0530

    Create Linked-List-Cycle-II.java

---
+++ Linked-List-Cycle-II.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set=new HashSet<ListNode>();
+        while(head!=null)
+        {
+            if(!set.add(head))
+                return head;
+            head=head.next;
+        }
+        return null;          
+    }
+}
